cb6962577d4cf9d651afd3499c0e972414a1701c
Royal Babu <123royalbabu@gmail.com>
2021-11-30 17:56:39 +0530

    connecting every thing

--- sql_data/models.py
+++ sql_data/models.py
@@ -16,8 +16,8 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-
-    items = relationship("Sheet", back_populates="owner")
+    
+    items = relationship("Sheet", back_populates="owner", uselist=False)
 
 
 
@@ -26,8 +26,8 @@ class Sheet(Base):
     __tablename__ = "items"
 
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(String, primary_key=True, index=True)
     title = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.email"))
+    owner_id = Column(String, ForeignKey("users.email"))
 
     owner = relationship("User", back_populates="items")
