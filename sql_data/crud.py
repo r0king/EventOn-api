1cdc1e0f16c9cf905282e2377e5662970dd9305c
Royal Babu <123royalbabu@gmail.com>
2021-11-30 17:56:39 +0530

    connecting every thing

--- sql_data/crud.py
+++ sql_data/crud.py
@@ -6,11 +6,18 @@ from . import models, schemas
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
-def get_sheet_of_user(db:Session, user:schemas.UserBase):
-    return db.query(models.Sheet).filter(models.Sheet.owner_id == user.id).all()
+def get_sheets_of_user(db:Session, user:schemas.UserBase):
+    # return db.query(models.Sheet).filter(models.Sheet.owner_id == user.email).all()
+    return db.query(models.Sheet).limit(10).all()
+
+def get_sheets_by_id(db:Session, id:str):
+    return db.query(models.Sheet).filter(models.Sheet.id == id).all()
 
 def create_user(db:Session,user:schemas.UserCreate):
     sample_hashed_pass = user.password + 'abcd'
@@ -20,9 +27,9 @@ def create_user(db:Session,user:schemas.UserCreate):
     db.refresh(new_user)
     return new_user
 
-def create_sheet(db:Session,sheet:schemas.Sheet,user_id:str):
+def create_sheet(db:Session,sheet:schemas.SheetName,user_id:str):
     
-    new_sheet = models.User(**sheet.dict(),owner_id=user_id)
+    new_sheet = models.Sheet(**sheet.dict(),owner_id=user_id)
     db.add(new_sheet)
     db.commit()
     db.refresh(new_sheet)
